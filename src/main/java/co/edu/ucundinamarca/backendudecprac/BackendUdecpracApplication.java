bbeae8e73d172feef2db1247ef978ef34114f615
andrezzzAlfarooox <alfarobrayan027@gmail.com>
2026-09-03 22:12:48 -0500

    feat(core): inicializacion del proyecto Spring Boot con Java 17 y PostgreSQL

---
+++ src/main/java/co/edu/ucundinamarca/backendudecprac/BackendUdecpracApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ucundinamarca.backendudecprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUdecpracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendUdecpracApplication.class, args);
+    }
+
+}
